01a69a3a0ae711088462958fb88096730740dc69
sdiels <32159048+sdiels@users.noreply.github.com>
2018-03-26 15:17:24 +0200

    mario animations

--- MarioSpaceKongGame/www/src/Game.js
+++ MarioSpaceKongGame/www/src/Game.js
@@ -1,48 +1,66 @@
-bootcamp.Game = function(game) {};
+bootcamp.Game = function (game) {};
 bootcamp.Game.prototype = {
-	create: function() {
-		this.physics.startSystem(Phaser.Physics.ARCADE);
+    create: function () {
+        this.physics.startSystem(Phaser.Physics.ARCADE);
 
 
-		this.player = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
-		this.player.anchor.set(0.5);
-		this.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+        this.player.anchor.set(0.5);
+        this.player.scale.setTo(10);
+        this.player.smoothed = false;
+        this.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.gravity.y = 600;
+        this.player.body.collideWorldBounds = true;
+        
+        this.player.animations.add('idle', [0], 1, true);
+        this.player.animations.add('walk', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('jump', [6], 1, true);
+        
+        this.bg = this.add.sprite(0, 0, 'bg');
 
-		this.keys = this.game.input.keyboard.createCursorKeys();
 
-		bootcamp._player = this.player;
-		window.addEventListener("deviceorientation", this.handleOrientation, true);
+        this.keys = this.game.input.keyboard.createCursorKeys();
 
-		this.borderGroup = this.add.group();
-		this.borderGroup.enableBody = true;
-		this.borderGroup.physicsBodyType = Phaser.Physics.ARCADE;
-		this.borderGroup.create(0, 0, 'border-horizontal');
-		this.borderGroup.create(0, bootcamp._HEIGHT-2, 'border-horizontal');
-		this.borderGroup.create(0, 0, 'border-vertical');
-		this.borderGroup.create(bootcamp._WIDTH-2, 0, 'border-vertical');
-		this.borderGroup.setAll('body.immovable', true);
-	},
-	update: function() {
-		if(this.keys.left.isDown) {
-			this.player.body.velocity.x -= this.movementForce;
-		}
-		else if(this.keys.right.isDown) {
-			this.player.body.velocity.x += this.movementForce;
-		}
-		if(this.keys.up.isDown) {
-			this.player.body.velocity.y -= this.movementForce;
-		}
-		else if(this.keys.down.isDown) {
-			this.player.body.velocity.y += this.movementForce;
-		}
-		this.physics.arcade.collide(this.player, this.borderGroup);
-	},
-	handleOrientation: function(e) {
-		// Device Orientation API
-		var x = e.gamma; // range [-90,90], left-right
-		var y = e.beta;  // range [-180,180], top-bottom
-		var z = e.alpha; // range [0,360], up-down
-		bootcamp._player.body.velocity.x += x;
-		bootcamp._player.body.velocity.y += y*0.5;
-	}
+        bootcamp._player = this.player;
+        window.addEventListener("deviceorientation", this.handleOrientation, true);
+
+    },
+    update: function () {
+        if (this.keys.left.isDown){
+            this.player.body.velocity.x = -200;
+        }
+        else if (this.keys.right.isDown){
+            this.player.body.velocity.x = 200;
+        }
+        else {
+            this.player.body.velocity.x = 0;
+        }
+        // Make the player jump if he is touching the ground
+        if (this.keys.up.isDown && this.player.body.blocked.down) {
+            this.player.body.velocity.y = -250;
+        }
+        
+        this.physics.arcade.collide(this.player, this.borderGroup);
+        
+        if(this.player.body.velocity.x == 0) {
+            this.player.animations.play('idle');
+        }else if (this.player.body.velocity.x > 0){
+            this.player.animations.play('walk');
+            this.player.scale.x = 10;
+        }else {
+            this.player.animations.play('walk');
+            this.player.scale.x = -10;
+        }
+        if(this.player.body.velocity.y < 0) {
+            this.player.animations.play('jump');
+        }
+    },
+    handleOrientation: function (e) {
+        // Device Orientation API
+        var x = e.gamma; // range [-90,90], left-right
+        var y = e.beta; // range [-180,180], top-bottom
+        var z = e.alpha; // range [0,360], up-down
+        bootcamp._player.body.velocity.x += x;
+        bootcamp._player.body.velocity.y += y * 0.5;
+    }
 };
--- MarioSpaceKongGame/www/src/Preloader.js
+++ MarioSpaceKongGame/www/src/Preloader.js
@@ -4,19 +4,10 @@ bootcamp.Preloader.prototype = {
 		this.preloadBg = this.add.sprite((bootcamp._WIDTH-297)*0.5, (bootcamp._HEIGHT-145)*0.5, 'preloaderBg');
 		this.preloadBar = this.add.sprite((bootcamp._WIDTH-158)*0.5, (bootcamp._HEIGHT-50)*0.5, 'preloaderBar');
 		this.load.setPreloadSprite(this.preloadBar);
-
-
-
-		this.load.image('title', 'img/title.png');
-		this.load.image('screen-mainmenu', 'img/screen-mainmenu.png');
-		this.load.image('screen-howtoplay', 'img/screen-howtoplay.png');
-		this.load.image('border-horizontal', 'img/border-horizontal.png');
-		this.load.image('border-vertical', 'img/border-vertical.png');
-
-		this.load.spritesheet('button-start', 'img/button-start.png', 146, 51);
         
         // MARIO ASSETS ///////////////////////////////////////////////////////////////////////////////////////////////////
         this.load.spritesheet('player', 'img/Mario.png', 17, 27, 7);
+        this.load.spritesheet('bg', 'img/bg.png', 516, 434, 8);
 	},
 	create: function() {
 		this.game.state.start('Game');
