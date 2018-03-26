6f02c1e576f97830d5b899147753ea4e86a8520e
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2018-03-26 14:38:34 +0200

    1

--- MarioSpaceKongGame/www/src/Preloader.js
+++ MarioSpaceKongGame/www/src/Preloader.js
@@ -4,6 +4,10 @@ bootcamp.Preloader.prototype = {
 		this.preloadBg = this.add.sprite((TestGame._WIDTH-297)*0.5, (TestGame._HEIGHT-145)*0.5, 'preloaderBg');
 		this.preloadBar = this.add.sprite((TestGame._WIDTH-158)*0.5, (TestGame._HEIGHT-50)*0.5, 'preloaderBar');
 		this.load.setPreloadSprite(this.preloadBar);
+        
+        //SPRITES
+        
+        game.load.spritesheet('enemy1', 'img/enemy-sprites.png', 6, 18, )
     },
     
 	create: function() {
