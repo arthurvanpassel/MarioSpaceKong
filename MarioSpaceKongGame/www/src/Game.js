47b5b81952e774e852efa3df54c9306b34f5d31e
RobinSel <robin.sel@student.kdg.be>
2018-03-26 17:10:11 +0200

    blabla

--- MarioSpaceKongGame/www/src/Game.js
+++ MarioSpaceKongGame/www/src/Game.js
@@ -2,10 +2,8 @@ bootcamp.Game = function(game) {};
 bootcamp.Game.prototype = {
 	create: function() {
 		this.physics.startSystem(Phaser.Physics.ARCADE);
-<<<<<<< HEAD
+
 		Phaser.Canvas.setSmoothingEnabled(this.game.context, false);
-=======
->>>>>>> 9a10c8f7f8821a57aba7ffa029c1f46cf0e4dabe
 
 		this.player = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
 		this.player.scale.setTo(1);
@@ -33,12 +31,10 @@ bootcamp.Game.prototype = {
 		this.barrel.body.immovable = true;
 		this.barrel.body.collideWorldBounds = true;
 
-		
+
 			_WIDTH = 200;
 			_HEIGHT = 200 * (window.innerHeight / window.innerWidth);
 
-
-<<<<<<< HEAD
 		var platforms = this.add.group();
 		platforms.enableBody = true;
 
@@ -47,15 +43,9 @@ bootcamp.Game.prototype = {
 		steel.scale.setTo(0.5);
 		//steel.body.immovable = true;
 
-=======
-		
 		var platforms = this.add.group();
 		//platforms.enableBody = true;
 
-		
-		
-		
->>>>>>> 9a10c8f7f8821a57aba7ffa029c1f46cf0e4dabe
 		//create random platforms
 		for(i = _HEIGHT/100; i < _HEIGHT;){
 
@@ -63,19 +53,16 @@ bootcamp.Game.prototype = {
 
 				var steel = platforms.create(w,i, 'steel');
 				steel.scale.setTo(0.5);
-			
+
 				//this.steel.body.immovable = true;
-<<<<<<< HEAD
 				steel.body.bounce.y = 0;
-    			steel.body.allowGravity = false;
-    			steel.body.collideWorldBounds = true;
+    		steel.body.allowGravity = false;
+    		steel.body.collideWorldBounds = true;
 				i +=0.5;
 				w +=20;
-=======
-				
+
 				i +=1.5;
 				w +=22;
->>>>>>> 9a10c8f7f8821a57aba7ffa029c1f46cf0e4dabe
 			};
 
 			i+=15;
